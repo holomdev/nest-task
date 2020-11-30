9b39b310f1528f5d4067d2edbb1df3500fb241cc
holomdev <holom.dev@gmail.com>
2020-11-30 16:15:26 -0600

    Custom pipe validating the task status

---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,23 @@
+import { BadRequestException, PipeTransform } from '@nestjs/common';
+import { TaskStatus } from '../task.model';
+
+export class TaskStatusValidationPipe implements PipeTransform {
+  readonly allowedStatuses = [
+    TaskStatus.OPEN,
+    TaskStatus.IN_PROGRESS,
+    TaskStatus.IN_PROGRESS,
+  ];
+
+  transform(value: any): any {
+    const status = value.toUpperCase();
+    if (!this.isStatusValid(status)) {
+      throw new BadRequestException(`"${status}" is an invalid status`);
+    }
+    return status;
+  }
+
+  private isStatusValid(status: any) {
+    const idx = this.allowedStatuses.indexOf(status);
+    return idx !== -1;
+  }
+}
